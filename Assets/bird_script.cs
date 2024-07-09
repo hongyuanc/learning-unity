593e7b1ef799d8ff643a92d066f53a291265182a
Hong <hong.cao0824@gmail.com>
2024-07-09 16:35:49 +0800

    game over when collision

--- Assets/bird_script.cs
+++ Assets/bird_script.cs
@@ -6,20 +6,27 @@ public class bird_script : MonoBehaviour
 {
     public Rigidbody2D body;
     public float scale = 5;
+    public logic_manager logic_Manager;
+    public bool status = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic_Manager = GameObject.FindGameObjectWithTag("Logic").GetComponent<logic_manager>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && status)
         {
             body.velocity = Vector2.up * scale;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        status = false;
+        logic_Manager.gameOver();
     }
 }
