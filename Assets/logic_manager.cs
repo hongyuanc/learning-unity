d179ac2123fad1b758538e51274bb1163aff8ca0
Hong <hong.cao0824@gmail.com>
2024-07-09 16:36:12 +0800

    set game over option

--- Assets/logic_manager.cs
+++ Assets/logic_manager.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class logic_manager : MonoBehaviour
 {
     public int score = 0;
     public Text playerScore;
+    public GameObject gameOverScene;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         score += scoreToAdd;
         playerScore.text = score.ToString();
+    }
+
+    public void resetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScene.SetActive(true);
     }
 }
