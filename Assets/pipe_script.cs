4ce6a01004d3060d9e8e53f3e0e185ccb3bc69d1
Hong <hong.cao0824@gmail.com>
2024-07-09 16:35:09 +0800

    remove pipes after leaving screen

--- Assets/pipe_script.cs
+++ Assets/pipe_script.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class pipe_script : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float deadZone = -9;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +18,11 @@ public class pipe_script : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe deleted");
+            Destroy(gameObject);
+        }
     }
 }
